e89277b0857bce024cdf2d0173cf9cbf51dba6c3
Микола. <basovmikola04@gmail.com>
2026-04-28 22:29:22 +0300

    Revert "copyed src from part1"
    This reverts commit 64f9ce70e0c7c7d639b90c73f0f1ee9d57ce004b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,26 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { useState, useEffect, useRef, useCallback } from 'react';
-import { getTodos } from './api/todos';
-import { Todo } from './types/Todo';
-import { FilterStatus } from './types/FilterStatus';
-import { Filter } from './types/Filter';
-
-import { Header } from './components/Header';
-import { TodoList } from './components/TodoList';
-import { Footer } from './components/Footer';
+import React from 'react';
 import { UserWarning } from './UserWarning';
-import { USER_ID } from './api/todos';
+
+const USER_ID = 0;
 
 export const App: React.FC = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [filterStatus, setFilterStatus] = useState<FilterStatus>(
-    FilterStatus.All,
-  );
-
-  const handleToggle = (id: number) => {
-    setTodos(prev =>
-      prev.map(todo =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
-      ),
-    );
-  };
-
-  const filters: readonly Filter[] = [
-    { label: 'All', value: FilterStatus.All },
-    { label: 'Active', value: FilterStatus.Active },
-    { label: 'Completed', value: FilterStatus.Completed },
-  ];
-
-  const filteredTodos = todos.filter(todo => {
-    if (filterStatus === FilterStatus.Active) {
-      return !todo.completed;
-    }
-
-    if (filterStatus === FilterStatus.Completed) {
-      return todo.completed;
-    }
-
-    return true;
-  });
-
-  const loadTodos = useCallback(() => {
-    setError(null);
-
-    return getTodos()
-      .then(setTodos)
-      .catch(() => setError('Unable to load todos'));
-  }, []);
-
-  const allCompleted = todos.length > 0 && todos.every(todo => todo.completed);
-  const activeTodosCount = todos.filter(todo => !todo.completed).length;
-  const hasCompleted = todos.some(todo => todo.completed);
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    loadTodos();
-    inputRef.current?.focus();
-  }, [loadTodos]);
-
-  useEffect(() => {
-    if (!error) {
-      return;
-    }
-
-    const timer = setTimeout(() => {
-      setError(null);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, [error]);
-
   if (!USER_ID) {
     return <UserWarning />;
   }
 
-  const handleToggleAll = () => {
-    const updatedTodos = todos.map(todo => ({
-      ...todo,
-      completed: !allCompleted,
-    }));
-
-    setTodos(updatedTodos);
-  };
-
   return (
-    <div className="todoapp">
-      <h1 className="todoapp__title">todos</h1>
+    <section className="section container">
+      <p className="title is-4">
+        Copy all you need from the prev task:
+        <br />
+        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
+          React Todo App - Load Todos
+        </a>
+      </p>
 
-      <div className="todoapp__content">
-        <Header
-          allCompleted={allCompleted}
-          onToggleAll={handleToggleAll}
-          inputRef={inputRef}
-        />
-
-        {todos.length > 0 && (
-          <TodoList todos={filteredTodos} onToggle={handleToggle} />
-        )}
-
-        {/* Hide the footer if there are no todos */}
-        {todos.length > 0 && (
-          <Footer
-            filters={filters}
-            filterStatus={filterStatus}
-            setFilterStatus={setFilterStatus}
-            activeTodosCount={activeTodosCount}
-            hasCompleted={hasCompleted}
-          />
-        )}
-      </div>
-      <div
-        data-cy="ErrorNotification"
-        className={`notification is-danger ${error ? '' : 'hidden'}`}
-      >
-        <button
-          data-cy="HideErrorButton"
-          type="button"
-          className="delete"
-          onClick={() => setError(null)}
-        />
-        {error}
-      </div>
-    </div>
+      <p className="subtitle">Styles are already copied</p>
+    </section>
   );
 };
--- src/types/Filter.ts
+++
@@ -1,6 +0,0 @@
-import { FilterStatus } from './FilterStatus';
-
-export type Filter = {
-  label: string;
-  value: FilterStatus;
-};
